a40120c2ef7f4f4e1d2820dea3befe9d0f950226
gui-freire <gui-freire@uol.com.br>
2018-01-23 19:55:14 -0200

    Criados DTOs e request do FCM

---
+++ noctuaback/src/main/java/Noctua/dto/Notification.java
@@ -0,0 +1,26 @@
+package Noctua.dto;
+
+public class Notification {
+	
+	private String title; //titulo da notificação
+	
+	private String body; //corpo da notificação
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	
+}
---
+++ noctuaback/src/main/java/Noctua/firebase/Firebase.java
@@ -0,0 +1,5 @@
+package Noctua.firebase;
+
+public class Firebase {
+
+}
