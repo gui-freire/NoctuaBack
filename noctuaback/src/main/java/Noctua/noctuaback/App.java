0c473bef743482e29ba92943b60b4ada513fb7be
gui-freire <gui-freire@uol.com.br>
2018-02-12 12:29:19 -0200

    Adicionada API do Google e chamadas para pegar AuthKey para Firebase

--- noctuaback/src/main/java/Noctua/noctuaback/App.java
+++ noctuaback/src/main/java/Noctua/noctuaback/App.java
@@ -1,13 +1,15 @@
 package Noctua.noctuaback;
 
+import Noctua.firebase.Firebase;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	Firebase firebase = new Firebase();
+	
+	public static void main(String[] args) {
+		Firebase.accessToken();
+	}
 }
